df117fec44297c73aa6150eaa10a248732d19b7d
rebeca.0902 <rebeca.0902@UEPT14-10652-E.aluno.pr.senac.br>
2022-05-26 16:35:21 -0300

    Hello Word

---
+++ castroturismo/br/com/rebeca/doamin/pessoa/Pessoa.java
@@ -0,0 +1,13 @@
+package br.com.rebeca.doamin.pessoa;
+
+public abstract class Pessoa {
+	
+	private Integer id;
+	
+	private String nome;
+	
+	private Pais pais;
+	
+	
+
+}
---
+++ castroturismo/src/main/java/br/com/rebeca/local/Pais.java
@@ -0,0 +1,74 @@
+package br.com.rebeca.local;
+
+public class Pais {
+	
+	private Integer id;
+	
+	private String nome;
+	
+	private Pais pais;
+
+	/**
+	 * 
+	 */
+	public Pais() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param id
+	 * @param nome
+	 * @param pais
+	 */
+	public Pais(Integer id, String nome, Pais pais) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.pais = pais;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public Integer getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the nome
+	 */
+	public String getNome() {
+		return nome;
+	}
+
+	/**
+	 * @param nome the nome to set
+	 */
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	/**
+	 * @return the pais
+	 */
+	public Pais getPais() {
+		return pais;
+	}
+
+	/**
+	 * @param pais the pais to set
+	 */
+	public void setPais(Pais pais) {
+		this.pais = pais;
+	}
+	
+
+}
